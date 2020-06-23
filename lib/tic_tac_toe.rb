2c3c5fcc3c814bf8429967673f99ee9b26adf255
Seth Near <seth.j.near@gmail.com>
2020-06-23 22:03:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ WIN_COMBINATIONS = [
   end
 
   def valid_move?(index)
-    if index.between?(0, 8) && !position_taken?(@board, index)
+    if index.between?(0, 8) && !position_taken?(index)
       return true
     else
       return false
